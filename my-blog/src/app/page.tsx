82c79334349b34a7ddf0eaac46719b137e13c1f3
Ayoub Abdoun <ayoob988@gmail.com>
2025-10-16 12:36:13 +0300

    auto deployment?

--- my-blog/src/app/page.tsx
+++ my-blog/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>Welcome to My Blog</h1>
       <p>This is my blog where you can see my work and stuff.</p>
+      <p>Testing purposes</p>
     </div>
   );
 }

1c7083daefd42a0578ca2f83d2fb92498ba01047
Ayoub Abdoun <ayoob988@gmail.com>
2025-10-16 11:10:22 +0300

    init

---
+++ my-blog/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to My Blog</h1>
+      <p>This is my blog where you can see my work and stuff.</p>
+    </div>
+  );
+}
